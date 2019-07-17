dd75f44208e269c0932418ea45472ae7ab80b5f0
Tom Becker <tjbecker@me.com>
2019-07-17 18:03:35 +0700

    working on making api work for watson

--- index.js
+++ index.js
@@ -1,21 +1,46 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const cors = require('cors')
+// Use our Watson library.
+var ToneAnalyzerV3 = require('watson-developer-cloud/tone-analyzer/v3');
 
-require('dotenv').config()
-require('./db')
-const app = express()
+// Require our config variables.
+var config = require('./config');
 
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
-app.use(cors())
+// The text that we want to analyze the tone of.
+var text = "In my younger and more vulnerable years my father gave me some advice that I’ve been turning over in my mind ever since. \“Whenever you feel like criticizing any one,\” he told me, \“just remember that all the people in this world haven’t had the advantages that you’ve had.\"";
+
+// Turn our text into valid json.
+var input = { "text": text };
+
+// The format that the tone analyzer needs.
+var params =
+        {
+        'tone_input': input,
+        'content_type': 'application/json'
+        };
+
+// Initialize the Tone Analyzer by giving it our credentials.
+const toneAnalyzer = new ToneAnalyzerV3({
+  version: '2017-09-21',
+  iam_apikey: '{apikey}',
+});
 
 
 
-app.get('/api/hashtag', require('./controllers/get_hashtag'))
+// Use our Tone Analyzer variable to analyze the tone.
+tone_analyzer.tone(params, function(error, response)
+        {
+        // There's an error.
+        if (error)
+                {
+                console.log('Error:', error);
+                }
+        // No error, we got our tone result.
+        else
+                {
+                // The tone of the text, as determined by watson.
+                var tone = JSON.stringify(response, null, 2)
 
-
-
-app.listen(process.env.PORT, () => {
-	console.log(`Server listening on port ${process.env.PORT}`);
-})
+                // Output Watson's tone analysis to the console.
+                console.log("The tone analysis for \'" + text + "\' is:\n");
+                console.log(tone);
+                }
+        });
