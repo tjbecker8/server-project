64ad47754df22a11de23d6c87f1a4a176df00019
Tom Becker <tjbecker@me.com>
2019-07-19 12:56:58 +0700

    added keyword api

---
+++ keyword.js
@@ -0,0 +1,18 @@
+const unirest = require('unirest')
+require('dotenv').config();
+
+
+const googleSpeechText = require('./googleSpeechText.js')
+
+googleSpeechText().then((transcription) => {
+
+unirest.post("https://textanalysis-keyword-extraction-v1.p.rapidapi.com/keyword-extractor-text")
+.header("X-RapidAPI-Host", "textanalysis-keyword-extraction-v1.p.rapidapi.com")
+.header("X-RapidAPI-Key", process.env.X_RAPIDAPI_Key)
+.header("Content-Type", "application/x-www-form-urlencoded")
+.send(`text=${transcription}`)
+.send("wordnum=5")
+.end(function (result) {
+  console.log(result.status, result.headers, result.body);
+});
+}).catch()
