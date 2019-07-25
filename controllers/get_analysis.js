54d2728edf19be4ac3e587db08356d45c80b6814
Tom Becker <tjbecker@me.com>
2019-07-25 11:12:04 +0700

    updated query

--- controllers/get_analysis.js
+++ controllers/get_analysis.js
@@ -1,9 +1,35 @@
 const db_fullAnalysis = require('../models/fullAnalysis')
 
 module.exports = (req, res) => {
-	db_fullAnalysis.find({}).then((data) => {
+
+	let q = {}
+	if (req.query && req.query._id) {
+		q._id = req.query._id
+	}
+
+	db_fullAnalysis.find(q).then((data) => {
 		res.send(data)
 	}).catch((err)=>{
 		res.send(err)
 	})
 }
+
+
+// module.exports = (req, res) => {
+// // console.log(req.query);
+// 	let q = {}
+// 	if (req.query && req.query.channel) {
+// 		q.channel = req.query.channel
+// 	}
+// 	db_message.find(q).populate({
+// 		path: 'channel',
+// 		select: 'channelName'
+// 	}).populate({
+// 		path: 'author',
+// 		select: 'email name'
+// 	}).sort('-date').then((data) => {
+// 		res.send(data)
+// 	}).catch((err)=>{
+// 		res.send(err)
+// 	})
+// }
