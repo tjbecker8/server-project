4b8afd0585b37343e15565ad1ccb444d67841032
Tom Becker <tjbecker@me.com>
2019-07-30 14:59:44 +0700

    removed stuff

--- controllers/get_analysis.js
+++ controllers/get_analysis.js
@@ -16,23 +16,3 @@ module.exports = (req, res) => {
 		res.send(err)
 	})
 }
-
-
-// module.exports = (req, res) => {
-// // console.log(req.query);
-// 	let q = {}
-// 	if (req.query && req.query.channel) {
-// 		q.channel = req.query.channel
-// 	}
-// 	db_message.find(q).populate({
-// 		path: 'channel',
-// 		select: 'channelName'
-// 	}).populate({
-// 		path: 'author',
-// 		select: 'email name'
-// 	}).sort('-date').then((data) => {
-// 		res.send(data)
-// 	}).catch((err)=>{
-// 		res.send(err)
-// 	})
-// }
