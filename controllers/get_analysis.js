e2aa2fac5a8cf6ede5752e6ac2197f70ff5cec80
Tom Becker <tjbecker@me.com>
2019-07-30 15:27:41 +0700

    added authentication

--- controllers/get_analysis.js
+++ controllers/get_analysis.js
@@ -1,11 +1,22 @@
 const db_fullAnalysis = require('../models/fullAnalysis')
+const jwt = require('jsonwebtoken')
+
+
 
 module.exports = (req, res) => {
+
+	let token = req.headers.authorization.split(' ')[1]
+	jwt.verify(token, process.env.SECRET, (err, decoded) =>{
+		if (decoded) {
+			console.log(decoded._id);
+
+
 
 	let q = {}
 	if (req.query && req.query._id) {
 		q._id = req.query._id
 	}
+
 
 	db_fullAnalysis.find(q).sort('date').populate({
 		path: 'author',
@@ -15,4 +26,7 @@ module.exports = (req, res) => {
 	}).catch((err)=>{
 		res.send(err)
 	})
+	}
+})
+
 }
